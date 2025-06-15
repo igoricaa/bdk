fd0be9fa9eb1a5878f4205997a41309ffa179937
Igor Stanisavljevic <stanisavljevic.igor@proton.me>
2025-06-15 15:07:44 +0200

    lenis, helvetica removal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { DM_Sans } from 'next/font/google';
 import './globals.css';
+import Lenis from '@/components/lenis';
 
 const dmSans = DM_Sans({
   subsets: ['latin'],
@@ -19,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={`${dmSans.variable} antialiased`}>{children}</body>
+      <body className={`${dmSans.variable} antialiased`}>
+        <Lenis>{children}</Lenis>
+      </body>
     </html>
   );
 }
