17a9eff91d79eeba60195b818a3516f8f5b46509
Igor Stanisavljevic <stanisavljevic.igor@proton.me>
2025-07-24 23:58:02 +0200

    smaller cursor

--- src/components/ui/custom-cursor.tsx
+++ src/components/ui/custom-cursor.tsx
@@ -12,9 +12,9 @@ const CustomCursor = () => {
         <Cursor
           follow
           center={{ x: 0.5, y: 0.5 }}
-          className='flex items-center justify-center min-w-25 min-h-25 max-w-25 max-h-25 p-4 rounded-full! bg-light-blue/90! backdrop-blur-sm'
+          className='flex items-center justify-center min-w-17 min-h-17 max-w-17 max-h-17 p-4 rounded-full! bg-light-blue/90! backdrop-blur-sm'
         >
-          <span className='text-white text-lg text-center leading-none'>
+          <span className='text-white text-sm text-center leading-none'>
             {zone === 'lawyers-card' ? 'View profile' : 'Read more'}
           </span>
         </Cursor>
